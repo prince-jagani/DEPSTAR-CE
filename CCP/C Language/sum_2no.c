f9440d9cd5ab9def0e74d7e7cce9df5f431a288d
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2022-09-29 00:41:10 +0530

    Add files via upload

---
+++ CCP/C Language/sum_2no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+
+    int i,j, sum=0;
+
+    printf("Enter First Number:");
+    scanf("%d", &i);
+    printf("Enter Second Number:");
+    scanf("%d", &j);
+
+    sum = i + j;
+
+    printf("Sum = %d", sum);
+}
