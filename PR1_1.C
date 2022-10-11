51b58a6b045b053ba20390b47fad0bde191c95e2
NEELDUDHAT <114645482+NEELDUDHAT@users.noreply.github.com>
2022-10-12 00:04:34 +0530

    Add files via upload

---
+++ PR1_1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a=1,b=4,c=3;
+
+	clrscr();
+	printf("%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c ",a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a);
+	printf("\n%c \"if you are resisting something,you are feeding it.\t\t\t%c ",b,b);
+	printf("\n%c\t any energy you fight,you are feeding it.\t\t\t%c ",c,c);
+	printf("\n%c\t\t if you are pushing something away,\t\t\t%c ",a,a);
+	printf("\n%c\t\t\t you are inviting it to stay.\"by Micheal singer.%c",b,b);
+	printf("\n%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c ",a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c,a,b,c);
+	getch();
+
+}
