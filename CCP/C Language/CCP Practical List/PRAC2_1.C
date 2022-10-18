0f5a4e0d18578adbe99ff52d7b3663bbdbe0fa22
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2022-10-18 23:19:20 +0530

    Add files via upload

---
+++ CCP/C Language/CCP Practical List/PRAC2_1.C
@@ -0,0 +1,30 @@
+ #include<stdio.h>
+ #include<conio.h>
+
+ void main()
+ {
+	  long p=80000;
+	  long m,w,l,lm,lw,ilm,ilw;
+
+	  clrscr();
+
+	  m=(p*52)/100;
+	  w=p-m;
+	  l=(p*48)/100;
+	  lm=(p*35)/100;
+	  lw=l-lm;
+	  ilm=m-lm;
+	  ilw=w-lw;
+
+	  printf("Total Population: %ld",p);
+	  printf("\nNumber of literate: %ld",l);
+	  printf("\nNumber of Men: %ld",m);
+	  printf("\nNumber of literate men: %ld",lm);
+	  printf("\nNumber of illiterate men: %ld",ilm);
+	  printf("\nNumber of Women: %ld",w);
+	  printf("\nNumber of literate women: %ld",lw);
+	  printf("\nNumber of illiterate women: %ld",ilw);
+
+	  getch();
+
+ }
