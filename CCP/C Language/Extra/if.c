c3723b6228947dbed5eb945664027fafe44d0cb4
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-10-20 00:49:22 +0530

    Add files via upload

---
+++ CCP/C Language/Extra/if.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,z;
+    // 1.
+   /*printf("THIS PROGRAM IS QESTION 1.");
+    printf("\nEnter number a : ");
+    scanf("%d",&a);
+    printf("\nEnter number b : ");
+    scanf("%d",&b);
+    if(a<b)
+    {
+        printf("\na(%d) is smaller than b(%d)",a,b);
+    }
+    else
+    {
+        printf("\nb(%d) is smaller than a(%d)",b,a);
+    }*/
+    
+
+    // 2.
+    printf("THIS PROGRAM IS QESTION 2.");
+    printf("\nEnter number : ");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("\nthis value %d is positive",a);
+    }
+    else
+    {
+        printf("\nthis value %d is negative",a);
+    }
+
+   
+
+
+}
