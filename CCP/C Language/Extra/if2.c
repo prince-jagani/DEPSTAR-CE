bc1e83bdaea1cc6d54c1b58cb3ffee7b443eb652
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2022-10-20 00:50:32 +0530

    Add files via upload

---
+++ CCP/C Language/Extra/if2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,z;
+    // 3.
+    printf("\nTHIS IPROGRAM IS QESTION 3.");
+    printf("\nEnter number : ");
+    scanf("%d",&a);
+    if (a%5==0)
+    {
+        printf("this value %d is divisible by 5",a);
+    }
+    else
+    {
+        printf("this value %d is not divisible by 5",a);
+    } 
+    
+    
+    
+  
+
+}
