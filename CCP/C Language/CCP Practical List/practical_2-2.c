c8876e11a339073f252b818bbf77330409146ae4
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2022-11-06 23:32:07 +0530

    Create practical_2-2.c

---
+++ CCP/C Language/CCP Practical List/practical_2-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10, b=50, c=100, t, nc, nb, na;
+	clrscr();
+	printf(“\t\t\t  22TDCS100\n”);
+	printf("Enter total amount : ");
+	scanf("%d",&t);
+	nc=t/c;
+	nb=(t-nc*100)/b;
+	na=(t-nc*100-nb*50)/a;
+	printf("\n100 notes is %d.",nc);
+	printf("\n50 notes is %d.",nb);
+	printf("\n10 notes is %d.",na);
+	getch();
+}
