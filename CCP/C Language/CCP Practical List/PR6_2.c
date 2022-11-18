9c335af11adda1924032c5a2df738efd1c919d67
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-11-18 13:35:56 +0530

    Add files via upload

---
+++ CCP/C Language/CCP Practical List/PR6_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a, b, i = 1, m = 1;
+    printf("enter num1:");
+    scanf("%d", &a);
+    printf("enter num2: ");
+    scanf("%d", &b);
+    while (i <= b)
+    {
+        m = m * a;
+        i++;
+        printf("%d ", m);
+    }
+    // printf("%d", m);
+}
