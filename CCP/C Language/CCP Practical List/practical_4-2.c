c640a1e7bad22874b215e50e698eeb08de840a46
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2022-11-18 12:54:35 +0530

    Update practical_4-2.c

--- CCP/C Language/CCP Practical List/practical_4-2.c
+++ CCP/C Language/CCP Practical List/practical_4-2.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include <conio.h>
+
 void main()
 {
-    int i = 1, n = 1;
-    printf("multification table\n");
-    do
-    {
+	int i=1,j=1;
+	char c;
+	clrscr();
 
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", n * i, n * i * 2, n * i * 3, n * i * 4, n * i * 5, n * i * 6, n * i * 7);
-        i++;
-    } while (i <= 10);
+	printf("\n\t  Multiplication Table (1 to 7)\n\n");
+
+       //	for(i=1;i<53;i++) printf("%c", 4);
+
+       while(i<53)
+       {
+		printf("%c",4);
+		i++;
+       }
+
+	printf("\n");
+
+	i=1;
+	do{
+	    printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",j*i,j*2*i,j*3*i,j*4*i,j*5*i,j*6*i,j*7*i);
+	    i++;
+	}while(i<=10);
+
+     /*	for(i=1; i<11; i++)
+	{
+		for(j=1; j<8; j++)
+		{
+			m=i*j;
+			printf("%d\t",m);
+		}
+		printf("\n");
+	}
+	*/
+	for(i=1;i<53;i++) printf("%c", 4);
+	getch();
 }
