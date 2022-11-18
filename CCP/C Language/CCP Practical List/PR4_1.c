f538fa35c2d0ffb181949dfc6035518bffd7eae9
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-11-18 12:50:46 +0530

    Add files via upload

---
+++ CCP/C Language/CCP Practical List/PR4_1.c
@@ -0,0 +1,23 @@
+/*
+    Dudhat Neel H.
+    ID=22TDCE039
+*/
+
+#include <stdio.h>
+void main()
+{
+    char name[25];
+    printf("ID:22DCE028\n\n");
+
+    // print name by scanf and printf
+
+    /* printf("enter your name:");
+     scanf("%s", name);
+     printf("%s\n", name);
+     */
+
+    // print name by gets and puts
+    printf("enter your name:\n");
+    gets(name);
+    puts(name);
+}
---
+++ CCP/C Language/CCP Practical List/PR4_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i = 1, n = 1;
+    printf("ID:22DCE028\n\n");
+    printf("multification table\n");
+    do
+    {
+
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", n * i, n * i * 2, n * i * 3, n * i * 4, n * i * 5, n * i * 6, n * i * 7);
+        i++;
+    } while (i <= 10);
+}
