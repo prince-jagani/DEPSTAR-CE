241b2d62cd8ac348e47a52a1dd0bfdee586e3dea
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-12-16 12:16:37 +0530

    Add files via upload

---
+++ CCP/C Language/CCP Practical List/PR6_4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int sticks = 21, pick, cp;
+    printf("\n\tID : 22DCE028\n");
+    for (int i = sticks; i > 0; i = i - pick)
+    {
+        printf("\n\nEnter your pick(1,2,3,4) : ");
+        scanf("%d", &pick);
+        if ((pick > sticks) || (pick > 4))
+        {
+            printf("wrong input");
+            break;
+        }
+        if ((sticks == 1) && (pick == 1))
+        {
+            printf("Computer won");
+
+            break;
+        }
+        cp = 5 - pick;
+        printf("\nComputer's pick : %d", cp);
+        sticks = sticks - pick - cp;
+        printf("\nRemaining sticks : %d", sticks);
+    }
+}
