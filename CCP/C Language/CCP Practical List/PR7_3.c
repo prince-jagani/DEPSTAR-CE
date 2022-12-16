18e931c9f26494ee07646fa423784824c1373cb7
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-12-16 13:17:36 +0530

    Add files via upload

---
+++ CCP/C Language/CCP Practical List/PR7_3.c
@@ -0,0 +1,65 @@
+// multification two 3*3 matrix
+
+#include <stdio.h>
+void main()
+{
+    int a[3][3], b[3][3], c[3][3], sum = 0;
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("first matrix:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nsecond matrix:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            sum = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                sum += a[i][k] * b[k][j];
+            }
+            c[i][j] = sum;
+        }
+    }
+    printf("multificaion of a and b matrix:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+}
