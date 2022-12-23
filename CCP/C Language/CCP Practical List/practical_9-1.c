88f8572e00929ba6f0c2ae8004baa3d0c09dca6e
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2022-12-23 12:30:31 +0530

    Create practical_9-1.c

---
+++ CCP/C Language/CCP Practical List/practical_9-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+    int n,i;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i=2; i<n/2+1; i++)
+    {
+        if(n%i==0)
+        {
+            printf("Number is not prime.");
+            i=0;
+            break;
+        }
+    }
+    if(i!=0)
+        printf("Number is prime.");
+}
