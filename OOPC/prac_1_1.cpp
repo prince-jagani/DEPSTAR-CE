132c86ae0d11c5a0b31a795e77c20a7eaecb6ea5
Prince Jagani <77915409+prince-jagani@users.noreply.github.com>
2023-03-07 18:42:07 +0530

    Add files via upload

---
+++ OOPC/prac_1_1.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Prince Jagani Local on 17-02-2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int _main11() {
+
+    for(int i=0; i< 41; i++)
+        cout<<"*";
+    cout<<endl;
+    printf("*\tPrograming Assignment 1\t\t*\n");
+    printf("*\t  Computer Programing 1\t\t*\n");
+    printf("*\t\tAuthor??\t\t*\n");
+    printf("*\tDue Date : Tuesday, Dec.20\t*\n");
+
+    for(int i=0; i< 41; i++)
+        cout<<"*";
+
+    return 0;
+}
